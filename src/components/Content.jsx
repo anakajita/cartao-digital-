c26dc7c5bf5a85022013000f4b16be7aad34529c
anakajita <kajitinhakn@gmail.com>
2025-04-18 16:20:10 -0300

    Add files via upload

---
+++ src/components/Content.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Content.css';
+
+function Content(){
+    return(
+        <>
+           <section>
+                <h1>Musa Kimura</h1>   
+                <h2>Musa é a Fada da Música no Clube das Winx. Ela vem do planeta Melodia e é apaixonada por tudo relacionado ao som e à melodia. Criativa, sensível e um pouco reservada, Musa expressa seus sentimentos por meio da música. É leal às amigas e tem um estilo mais alternativo, com um visual inspirado no mundo pop e rock.</h2> 
+
+                
+                <img 
+                    src="https://th.bing.com/th/id/R.d7ba4e6717f93aced223a2480bda8718?rik=HUdyE%2bSvMh7T1Q&riu=http%3a%2f%2fimages5.fanpop.com%2fimage%2fphotos%2f28000000%2fmusa-winx-club-enchantix-28059488-536-578.png&ehk=vISpzQRmycrA0EEY7roGzOwh9X2sFqj4qrQyRbpUjbk%3d&risl=&pid=ImgRaw&r=0" 
+                    alt="imagem" 
+                    className="imagem"
+            
+                />   
+                <h3>Seus poderes em Enchantix são o Ataque de Ondas Sonoras, Gaiola de Som, Percussão Enchantix.</h3>
+
+                <img 
+                    src="https://i.pinimg.com/736x/63/f5/33/63f533786b1c2c00b93eb26c79ce395a.jpg"
+                    className="imagem"
+                />   
+                <h4>Poder no Charmix: Volume Máximo feitiço sonoro poderoso de Musa que emite uma onda de som intensa para desorientar ou afastar inimigos.</h4>
+               
+                <img 
+                    src="https://th.bing.com/th/id/R.b1c03b5e6c61fa2f373adf234e2c7272?rik=SL01Tb08FIxMFA&riu=http%3a%2f%2fvignette1.wikia.nocookie.net%2fwinx%2fimages%2f8%2f82%2fMusa_sophix_by_bloom2-d4rjwq0.png%2frevision%2flatest%3fcb%3d20141206113501%26path-prefix%3dpl&ehk=ro1ptRS2S%2bBzHcAgD30AXo8nFTZ3%2bn3S%2fY%2fFT4z3bUk%3d&risl=&pid=ImgRaw&r=0" 
+                    alt="imagem" 
+                    className="imagem"
+            
+                />   
+                <h5>Harmonia Pura é uma energia musical de Musa que traz equilíbrio à natureza na transformação Sophix.</h5>
+
+
+                
+           </section> 
+        </>
+    );
+};
+export default Content;
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <>
+            <header className="header">
+                <img 
+                    src="https://i.pinimg.com/originals/05/eb/c0/05ebc0ff903f32369fb1bbad5f3ef6cb.jpg" 
+                    alt="Banner do Header" 
+                    className="header-image" 
+                />
+            </header>
+        </>
+    );
+};
+
+export default Header;
